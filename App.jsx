817eec428bc528c1e1c13ac70b688783645617a1
Biswas-Souvik <biswassouvik284@gmail.com>
2025-06-17 22:12:33 +0530

    Keyboard Section

--- App.jsx
+++ App.jsx
@@ -1,21 +1,10 @@
 import { useState } from "react";
 import { languages } from "./languages";
 
-/**
- * Goal: Build out the main parts of our app
- *
- * Challenge:
- * 1. Save a "currentWord" in state. Initialize as "react".
- * 2. Map over the letters of the word (you'll need to turn
- *    the string into an array of letters first) and display
- *    each one as a <span>. Capitalize the letters when
- *    displaying them.
- * 3. Style to look like the design. You can get the underline
- *    effect on the box using `border-bottom`.
- */
-
 export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   const languageElements = languages.map((lang) => {
     const styles = {
@@ -33,6 +22,10 @@ export default function AssemblyEndgame() {
     .split("")
     .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
 
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+
   return (
     <main>
       <header>
@@ -48,6 +41,8 @@ export default function AssemblyEndgame() {
       </section>
       <section className="language-chips">{languageElements}</section>
       <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
     </main>
   );
 }
