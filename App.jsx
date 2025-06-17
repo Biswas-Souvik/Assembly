028dc7c38a848a84226d7f8b9ca18665aecfc171
Biswas-Souvik <biswassouvik284@gmail.com>
2025-06-17 13:07:23 +0530

    Status section

--- App.jsx
+++ App.jsx
@@ -5,6 +5,10 @@ export default function AssemblyEndgame() {
   return (
     <main>
       <Header />
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
     </main>
   );
 }
