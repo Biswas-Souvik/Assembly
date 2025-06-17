e1b77012b9783f0999a5494e8be92607bdf9a4d2
Biswas-Souvik <biswassouvik284@gmail.com>
2025-06-17 13:24:44 +0530

    languages sections

--- App.jsx
+++ App.jsx
@@ -1,14 +1,33 @@
 import React from "react";
-import Header from "./Header";
+import { languages } from "./languages";
 
 export default function AssemblyEndgame() {
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.n}>
+        {lang.name}
+      </span>
+    );
+  });
+
   return (
     <main>
-      <Header />
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
       <section className="game-status">
         <h2>You win!</h2>
         <p>Well done! 🎉</p>
       </section>
+      <section className="language-chips">{languageElements}</section>
     </main>
   );
 }
