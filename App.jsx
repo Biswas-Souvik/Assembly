0a01cd3831d183fbf2d8caec16728b56581f0dc4
Biswas-Souvik <biswassouvik284@gmail.com>
2025-08-09 18:34:49 +0530

    wrong guess count

--- App.jsx
+++ App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
-import clsx from "clsx";
+import { clsx } from "clsx";
 import { languages } from "./languages";
 
 export default function AssemblyEndgame() {
+  // State values
   const [currentWord, setCurrentWord] = useState("react");
   const [guessedLetters, setGuessedLetters] = useState([]);
 
+  // Derived values
+  const wrongGuessCount = guessedLetters.filter(
+    (letter) => !currentWord.includes(letter)
+  ).length;
+
+  // Static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   function addGuessedLetter(letter) {
@@ -37,15 +44,15 @@ export default function AssemblyEndgame() {
   const keyboardElements = alphabet.split("").map((letter) => {
     const isGuessed = guessedLetters.includes(letter);
     const isCorrect = isGuessed && currentWord.includes(letter);
-    const isWrong = isGuessed && !isCorrect;
-    const keyClassName = clsx({
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({
       correct: isCorrect,
       wrong: isWrong,
     });
 
     return (
       <button
-        className={keyClassName}
+        className={className}
         key={letter}
         onClick={() => addGuessedLetter(letter)}
       >
