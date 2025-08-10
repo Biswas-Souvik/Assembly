14c07c86094c3e4244f8ff081bf4d09e01b2e35f
Biswas-Souvik <biswassouvik284@gmail.com>
2025-08-10 18:59:13 +0530

    Display Won/Lost Status

--- App.jsx
+++ App.jsx
@@ -68,6 +68,11 @@ export default function AssemblyEndgame() {
     );
   });
 
+  const gameStatusClass = clsx("game-status", {
+    won: isGameWon,
+    lost: isGameLost,
+  });
+
   return (
     <main>
       <header>
@@ -78,9 +83,20 @@ export default function AssemblyEndgame() {
         </p>
       </header>
 
-      <section className="game-status">
-        <h2>You win!</h2>
-        <p>Well done! 🎉</p>
+      <section className={gameStatusClass}>
+        {isGameOver ? (
+          isGameWon ? (
+            <>
+              <h2>You win!</h2>
+              <p>Well done! 🎉</p>
+            </>
+          ) : (
+            <>
+              <h2>Game over!</h2>
+              <p>You lose! Better start learning Assembly 😭</p>
+            </>
+          )
+        ) : null}
       </section>
 
       <section className="language-chips">{languageElements}</section>
